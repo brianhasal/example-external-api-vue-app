8be241f07cfdaf84fa65ecf3d759df79a2322e5a
Brian Hasal <brian.hasal@gmail.com>
2022-06-20 18:05:20 -0700

    Finsihed

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,13 +1,30 @@
 <script>
-  import axios from "axios"
-  
+  import axios from "axios";
+
   export default {
     data: function () {
       return {
         message: "Welcome to Vue.js!",
+        posts: [],
+        articles: [],
       };
     },
-    created: function () {},
+    created: function () {
+      axios.get("https://jsonplaceholder.typicode.com/posts").then(response => {
+        console.log(response.data);
+        this.posts = response.data;
+      });
+
+      // axios.get("https://newsapi.org/v2/top-headlines?country=us&apiKey=____").then(response => {
+      //   console.log(response.data);
+      //   this.articles = response.data;
+      // });
+
+      axios.get("/articles").then((response) => {
+        console.log(response.data);
+        this.articles = response.data;
+      });
+    },
     methods: {},
   };
 </script>
@@ -15,6 +32,13 @@
 <template>
   <div class="home">
     <h1>{{ message }}</h1>
+    <div v-for="post in posts" v-bind:key="post.id">
+      <h2>{{ post.title }}</h2>
+      <p>{{ post.body }}</p>
+    </div>
+    <div v-for="article in articles" :key="article.title">
+      <h2>{{ article }}</h2>
+    </div>
   </div>
 </template>
 
